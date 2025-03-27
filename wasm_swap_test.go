c83870e466da14f7cf7ef4c90f0614b80e462cd8
xiemylogos <xiemylogos@gmail.com>
2025-03-27 10:27:39 +0800

    update test code

--- wasm_swap_test.go
+++ wasm_swap_test.go
@@ -91,7 +91,7 @@ func TestPauseSwapWasmContract(t *testing.T) {
 	assert.Nil(t, err)
 	gasprice := uint64(2500)
 	invokegaslimit := uint64(200000)
-	contractAddr, err := common.AddressFromHexString("59dcfb1fb5bcc3679e6164f96724fc3baedd2db2") //wasmoep4 bridge
+	contractAddr, err := common.AddressFromHexString("2193b435eb7153dfa844edb2867dd9a66058b0c4") //wasmoep4 bridge
 	assert.Nil(t, err)
 	txHash, err := testOntSdk.WasmVM.InvokeWasmVMSmartContract(
 		gasprice, invokegaslimit, nil, testDefAcc, contractAddr, "pause", []interface{}{})
@@ -160,7 +160,7 @@ func TestRegisterTokenPairSwapWasmContract(t *testing.T) {
 	assert.Nil(t, err)
 	gasprice := uint64(2500)
 	invokegaslimit := uint64(200000)
-	contractAddr, err := common.AddressFromHexString("59dcfb1fb5bcc3679e6164f96724fc3baedd2db2") //wasm uniswap v2
+	contractAddr, err := common.AddressFromHexString("2193b435eb7153dfa844edb2867dd9a66058b0c4") //wasm uniswap v2
 	assert.Nil(t, err)
 	evmUniswapAddr, err := common.AddressFromHexString("b1ecc98a3ffedb9bd652981df286b1e971e6cf12") //evm swap contract 0x12cfe671e9b186f21d9852d69bdbfe3f8ac9ecb1
 	assert.Nil(t, err)
@@ -183,7 +183,7 @@ func TestUnRegisterTokenPairSwapWasmContract(t *testing.T) {
 	assert.Nil(t, err)
 	gasprice := uint64(2500)
 	invokegaslimit := uint64(200000)
-	contractAddr, err := common.AddressFromHexString("59dcfb1fb5bcc3679e6164f96724fc3baedd2db2") //wasm uniswap
+	contractAddr, err := common.AddressFromHexString("2193b435eb7153dfa844edb2867dd9a66058b0c4") //wasm uniswap
 	assert.Nil(t, err)
 	tokenPairName := "ONG_TO_WONT"
 	txHash, err := testOntSdk.WasmVM.InvokeWasmVMSmartContract(
@@ -198,10 +198,10 @@ type SwapTokenPair struct {
 	ContractAddr common.Address
 }
 
-func TestGeTokenPairSwapWasmContract(t *testing.T) {
+func TestGetTokenPairSwapWasmContract(t *testing.T) {
 	testOntSdk = NewOntologySdk()
 	testOntSdk.NewRpcClient().SetAddress(testNetUrl)
-	contractAddr, err := common.AddressFromHexString("59dcfb1fb5bcc3679e6164f96724fc3baedd2db2") //wasm uniswapv2
+	contractAddr, err := common.AddressFromHexString("2193b435eb7153dfa844edb2867dd9a66058b0c4") //wasm uniswapv2
 	assert.Nil(t, err)
 	tokenPairName := "ONG_TO_WONT"
 	res, err := testOntSdk.WasmVM.PreExecInvokeWasmVMContract(
@@ -226,7 +226,7 @@ func TestGeTokenPairSwapWasmContract(t *testing.T) {
 func TestGetAllTokenPairSwapWasmContract(t *testing.T) {
 	testOntSdk = NewOntologySdk()
 	testOntSdk.NewRpcClient().SetAddress(testNetUrl)
-	contractAddr, err := common.AddressFromHexString("59dcfb1fb5bcc3679e6164f96724fc3baedd2db2") //wasm uniswap
+	contractAddr, err := common.AddressFromHexString("2193b435eb7153dfa844edb2867dd9a66058b0c4") //wasm uniswap
 	assert.Nil(t, err)
 	res, err := testOntSdk.WasmVM.PreExecInvokeWasmVMContract(
 		contractAddr, "getAllTokenPairName", []interface{}{})
@@ -261,14 +261,14 @@ func TestSwapExactTokensForTokensSwapWasmContract(t *testing.T) {
 	assert.Nil(t, err)
 	gasprice := uint64(2500)
 	invokegaslimit := uint64(200000)
-	contractAddr, err := common.AddressFromHexString("59dcfb1fb5bcc3679e6164f96724fc3baedd2db2") //wasm uniswap v2
+	contractAddr, err := common.AddressFromHexString("2193b435eb7153dfa844edb2867dd9a66058b0c4") //wasm uniswap v2
 	assert.Nil(t, err)
 	tokenPairName := "ONG_TO_WONT"
 	wong, err := common.AddressFromHexString("a5a62d83759150d6465ed68b15c9eb9f02397219") //0x197239029febc9158bd65e46d6509175832da6a5
 	assert.Nil(t, err)
 	wont, err := common.AddressFromHexString("193c28b10b0f778d007744f1249520389061e54c") //0x4ce5619038209524f14477008d770f0bb1283c19
 	assert.Nil(t, err)
-	toAddr, err := common.AddressFromHexString("357be1f9e1c98b83f1ca1e363b79743019936d0f")
+	toAddr, err := common.AddressFromHexString("0f6d93193074793b361ecaf1838bc9e1f9e17b35") //0x357be1f9e1c98b83f1ca1e363b79743019936d0f
 	assert.Nil(t, err)
 	ethAddr, err := common.AddressFromHexString("25350e95e87d0b385d82e42b43f0ef272b484b5f") //0x5f4b482b27eff0432be4825d380b7de8950e3525
 	assert.Nil(t, err)
